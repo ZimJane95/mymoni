100384bd8a9e7a77f09e50c05059e4c374e9f076
Jane Ibezim <ibezimjane4@gmail.com>
2022-11-23 10:41:43 +0100

    Created som Data

---
+++ utilities/AreaView.js
@@ -0,0 +1,16 @@
+import { SafeAreaView, Platform, StatusBar, StyleSheet } from "react-native";
+
+export function SafeArea(children){
+    return(
+        <SafeAreaView style={styles.container}>
+
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        
+    }
+
+})
